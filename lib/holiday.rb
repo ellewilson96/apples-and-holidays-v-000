bd8ab9643886d6111d6b582c149cdad911f71b29
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 18:28:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,8 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    season.
-    holiday.
+    season
+    holiday
   end
 end
 
