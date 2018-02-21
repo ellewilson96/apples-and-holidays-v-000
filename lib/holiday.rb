e98cfd2f7246afb88eca8629003237f5c9a0d7e1
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 19:02:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |name, supplies|
     name.to_s.capitalize!
     name.values.to_s.capitalize!
-    puts "#{name}:" name.values
+    puts "#{name}:"
 end
 end
 end
