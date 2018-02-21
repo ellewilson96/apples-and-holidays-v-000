aae3ddd4a33b2287b1282ee910ded0d977c71269
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-21 18:54:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,6 @@ def all_supplies_in_holidays(holiday_hash)
     puts holiday.to_s.capitalize!+":"
 end
 end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
